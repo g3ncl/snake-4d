6c905f43a3a487694410df98c8e3315bff53b926
Claudio Genovese <18346630+g3ncl@users.noreply.github.com>
2024-07-09 12:20:59 +0200

    Added assetPrefix in next.config to load static assets correctly when using proxy redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 import path from "path";
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(new URL(import.meta.url).pathname, "styles")],
   },
+  assetPrefix: isProd ? "https://snake4d.netlify.app/" : undefined,
 };
 
 export default nextConfig;
