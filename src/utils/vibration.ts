54362adc7df109fc06a43d857baa28a65ca541d3
Claudio Genovese <18346630+g3ncl@users.noreply.github.com>
2025-11-23 13:23:22 +0100

    fix: change vibration default pattern

--- src/utils/vibration.ts
+++ src/utils/vibration.ts
@@ -1,4 +1,4 @@
-export const triggerVibration = (pattern: number | number[] = 200): void => {
+export const triggerVibration = (pattern: number | number[] = 50): void => {
   if (typeof navigator !== "undefined" && "vibrate" in navigator) {
     navigator.vibrate(pattern);
   }
