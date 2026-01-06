fadff3762497b8443853636c171103876c29421d
Claudio Genovese <genovese.claudio997@gmail.com>
2026-01-06 18:23:02 +0100

    chore: use netlify set url variable in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 import path from "path";
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(new URL(import.meta.url).pathname, "styles")],
   },
-  assetPrefix: isProd ? "https://snake4d.netlify.app/" : undefined,
+  assetPrefix: process.env.URL,
 };
 
 export default nextConfig;
